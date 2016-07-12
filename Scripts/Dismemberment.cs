3447c23d45bb9dc53df6076b0a94b94ead92ded1
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-07-12 13:12:28 +0800

    Added dismemberment script and player projectile bheaviors

---
+++ Scripts/Dismemberment.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dismemberment : MonoBehaviour {
+	private bool isAlive;
+	private int bodyHP;
+	// Use this for initialization
+	void Start () {
+		isAlive=true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody[] rb=gameObject.GetComponentsInChildren<Rigidbody>();
+			
+			foreach(Rigidbody JointRB in rb){
+				if(JointRB.gameObject.transform.parent!=null&&isAlive||getAlive()){
+						JointRB.isKinematic=true;
+			}else
+				if(!isAlive||getAlive()){
+					JointRB.isKinematic=false;
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+			isAlive=false;
+		if(Input.GetKeyDown(KeyCode.Alpha2))
+			isAlive=true;
+
+		Debug.Log("Value from model: isAlive "+isAlive+" getAlive"+getAlive());
+	}
+	public void setAlive(bool isAlive){
+		this.isAlive=isAlive;
+	}
+	public bool getAlive(){
+		return isAlive;
+	}
+}
---
+++ Scripts/PlayerProjectile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProjectile : MonoBehaviour {
+	Dismemberment DismemberReference;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag=="Enemy"){
+			Rigidbody[] rb=col.gameObject.GetComponentsInParent<Rigidbody>();
+		//	DismemberReference=col.gameObject.GetComponentInParent<Dismemberment>();
+			foreach(Rigidbody JointRB in rb){
+				if(JointRB.gameObject.transform.parent!=null)
+					Debug.Log (JointRB.name);
+				}
+			Debug.Log(col.gameObject.name);
+		//	Debug.Log("setAlive Value"+DismemberReference.getAlive());
+		}
+
+		Destroy(gameObject);
+	}
+}

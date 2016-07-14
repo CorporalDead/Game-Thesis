e67d9f48aac50c9398cad028ff0856abf5a5ced3
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-07-14 19:25:16 +0800

    Added ability to dismember for projectiles

--- Scripts/PlayerProjectile.cs
+++ Scripts/PlayerProjectile.cs
@@ -2,7 +2,23 @@
 using System.Collections;
 
 public class PlayerProjectile : MonoBehaviour {
-	Dismemberment DismemberReference;
+	public GameObject ForearmGib;
+	public GameObject ShoulderGib;
+	public GameObject ArmGib;
+
+	public GameObject RightForearmGib;
+	public GameObject RightShoulderGib;
+	public GameObject RightArmGib;
+
+	private GameObject LeftForearm;
+	private GameObject LeftShoulder;
+	private GameObject LeftArm;
+
+	private GameObject RightForearm;
+	private GameObject RightShoulder;
+	private GameObject RightArm;
+
+	private Dismemberment DismemberReference;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,6 +30,7 @@ public class PlayerProjectile : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision col){
+		//collision for enemies
 		if(col.gameObject.tag=="Enemy"){
 			Rigidbody[] rb=col.gameObject.GetComponentsInParent<Rigidbody>();
 			foreach(Rigidbody JointRB in rb){
@@ -23,12 +40,31 @@ public class PlayerProjectile : MonoBehaviour {
 
 						DismemberReference.setAlive(false);
 						DismemberReference.setBodyHP(DismemberReference.getHP()-1);
-					}
-				}
-			if(DismemberReference.getHP()==0)
-			Destroy(col.gameObject);
-		}
 
-		Destroy(gameObject);
+				
+				if(DismemberReference.getHP()==0){
+					if(col.gameObject.name=="LeftForearm"){
+						LeftForearm=Instantiate(ForearmGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+						DismemberReference.setLeftForearmActive(true);
+					}
+					if(col.gameObject.name=="LeftClavicle"){
+						if(DismemberReference.getLeftForearmActive()==false)
+							LeftArm=Instantiate(ArmGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+					}
+
+					if(col.gameObject.name=="RightForearm"){
+							RightForearm=Instantiate(ForearmGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;		
+					}
+					if(col.gameObject.name=="RightClavicle"){	
+							RightArm=Instantiate(ArmGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+						}
+					
+					Destroy(col.gameObject);
+				}
+			}
+		}
+	}
+
+		//Destroy(gameObject);
 	}
 }

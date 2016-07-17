03cae7b3a2e6fa7d41e0419688fa1ac7dd5beb32
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-07-17 21:01:21 +0800

    Added reference to PauseMenu.cs
    Also prevent the player from looking around while the game is paused and enabling the cursor.

--- Scripts/MovementScript.cs
+++ Scripts/MovementScript.cs
@@ -5,7 +5,9 @@ using System.Collections;
 public class MovementScript : MonoBehaviour {
 	public GameObject ProjectileTest;
 	public GameObject WeaponOnHand;
-	
+
+	private PauseMenu PauseMenuReference;
+
 	private CharacterController playerController;
 	private Transform playerTransform;
 	private Camera playerCamera;
@@ -38,7 +40,9 @@ public class MovementScript : MonoBehaviour {
 		playerController=GetComponent<CharacterController>();
 		playerCamera=gameObject.GetComponentInChildren<Camera>();
 		playerCamGO=GameObject.Find("PlayerCamera");
-		
+
+		PauseMenuReference=GetComponent<PauseMenu>();
+
 		FieldOfViewValue=playerCamera.fieldOfView;
 		Direction=Vector3.zero;
 		playerTransform=transform;
@@ -50,133 +54,136 @@ public class MovementScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {	
-		float ForwardBackward=Input.GetAxis("Vertical")*playerSpeed;
-		float LeftRight=Input.GetAxis("Horizontal")*playerSpeed;
- 
 
-		playerRotX+=Input.GetAxis("Mouse Y")*1f;
-		//correcting rotation so player will not bend over (and break their necks)
-		if(playerRotX>85&&playerCamGO.transform.localEulerAngles.x>270)
-			playerRotX-=Input.GetAxis("Mouse Y")*1f;
-		else
-			if(playerRotX<-85&&playerCamGO.transform.localEulerAngles.x>85)
+		if(!PauseMenuReference.getPaused()){
+			float ForwardBackward=Input.GetAxis("Vertical")*playerSpeed;
+			float LeftRight=Input.GetAxis("Horizontal")*playerSpeed;
+	 
+
+			playerRotX+=Input.GetAxis("Mouse Y")*1f;
+			//correcting rotation so player will not bend over (and break their necks)
+			if(playerRotX>85&&playerCamGO.transform.localEulerAngles.x>270)
 				playerRotX-=Input.GetAxis("Mouse Y")*1f;
-		
-		playerRotY+=Input.GetAxis("Mouse X")*1f;
-		
-		if(playerController.isGrounded){
-			//Character Sprint
-			if(Input.GetButton("Sprint")&&ForwardBackward>0){
-				ForwardBackward+=playerSpeed;
+			else
+				if(playerRotX<-85&&playerCamGO.transform.localEulerAngles.x>85)
+					playerRotX-=Input.GetAxis("Mouse Y")*1f;
+			
+			playerRotY+=Input.GetAxis("Mouse X")*1f;
+			
+			if(playerController.isGrounded){
+				//Character Sprint
+				if(Input.GetButton("Sprint")&&ForwardBackward>0){
+					ForwardBackward+=playerSpeed;
+					
+					if(playerCamera.fieldOfView<FieldOfViewValue+10f)
+						playerCamera.fieldOfView+=0.5f;
+
+				}else{
+					if(playerCamera.fieldOfView>FieldOfViewValue)
+						playerCamera.fieldOfView-=0.5f;
+				}
 				
-				if(playerCamera.fieldOfView<FieldOfViewValue+10f)
-					playerCamera.fieldOfView+=0.5f;
+				Direction.y=0;
+				Direction=new Vector3(LeftRight,0,ForwardBackward);
+				Direction=transform.TransformDirection(Direction);
+				
+				//Character Jump
+				if(Input.GetButtonDown("Jump")&&(!isWallRunningLeft||!isWallRunningRight)){
+					Direction.y+=5f;
+					isJumping=true;
+				}
 
 			}else{
-				if(playerCamera.fieldOfView>FieldOfViewValue)
-					playerCamera.fieldOfView-=0.5f;
-			}
-			
-			Direction.y=0;
-			Direction=new Vector3(LeftRight,0,ForwardBackward);
-			Direction=transform.TransformDirection(Direction);
-			
-			//Character Jump
-			if(Input.GetButtonDown("Jump")&&(!isWallRunningLeft||!isWallRunningRight)){
-				Direction.y+=5f;
-				isJumping=true;
-			}
+				if(!isWallRunning){
+					Direction.y-=20f*Time.deltaTime;
+					isJumping=false;
+					runTimer=0.5f;
 
-		}else{
-			if(!isWallRunning){
-				Direction.y-=20f*Time.deltaTime;
-				isJumping=false;
-				runTimer=0.5f;
-
-				if(playerCamGO.transform.localEulerAngles.z>=345f&&playerCamGO.transform.localEulerAngles.z!=0f)
-					playerCamGO.transform.Rotate(0,0,1);
-				//check if player was wallrunning on the right side then correct the rotation
-				if(playerCamGO.transform.localEulerAngles.z<=345f&&playerCamGO.transform.localEulerAngles.z!=0f)
-					playerCamGO.transform.Rotate(0,0,-1);
-			}else{
-				if(isWallRunningLeft){
+					if(playerCamGO.transform.localEulerAngles.z>=345f&&playerCamGO.transform.localEulerAngles.z!=0f)
+						playerCamGO.transform.Rotate(0,0,1);
+					//check if player was wallrunning on the right side then correct the rotation
+					if(playerCamGO.transform.localEulerAngles.z<=345f&&playerCamGO.transform.localEulerAngles.z!=0f)
+						playerCamGO.transform.Rotate(0,0,-1);
+				}else{
+					if(isWallRunningLeft){
+						if(Input.GetButtonDown("Jump")){
+							//Debug.Log("Jump On");
+							jumpedFromLeft=true;
+							
+							Direction.y+=2f;
+							
+							Direction.x=0;
+							Direction.z=0;
+							Direction.x+=5f;
+							Direction.z+=5f;
+							Direction = transform.TransformDirection(Direction);
+					}
+						if((playerCamGO.transform.localEulerAngles.z>=0f&&playerCamGO.transform.localEulerAngles.z<=1f)||playerCamGO.transform.localEulerAngles.z>=350f)
+							playerCamGO.transform.Rotate(0,0,-1);
+						//decrement runtimer so player does not wall run forever
+					runTimer-=Time.deltaTime;
+				}else
+				if(isWallRunningRight){
 					if(Input.GetButtonDown("Jump")){
-						//Debug.Log("Jump On");
-						jumpedFromLeft=true;
+							//Debug.Log("Jump off");
+						jumpedFromRight=true;
 						
 						Direction.y+=2f;
 						
 						Direction.x=0;
 						Direction.z=0;
-						Direction.x+=5f;
+						Direction.x-=5f;
 						Direction.z+=5f;
 						Direction = transform.TransformDirection(Direction);
+					}
+						//tilt camera to the right
+						if((playerCamGO.transform.localEulerAngles.z>=359f&&playerCamGO.transform.localEulerAngles.z<=359.9f)||playerCamGO.transform.localEulerAngles.z<=10f)
+							playerCamGO.transform.Rotate(0,0,1);
+
+					runTimer-=Time.deltaTime;
 				}
-					if((playerCamGO.transform.localEulerAngles.z>=0f&&playerCamGO.transform.localEulerAngles.z<=1f)||playerCamGO.transform.localEulerAngles.z>=350f)
-						playerCamGO.transform.Rotate(0,0,-1);
-					//decrement runtimer so player does not wall run forever
-				runTimer-=Time.deltaTime;
-			}else
-			if(isWallRunningRight){
-				if(Input.GetButtonDown("Jump")){
-						//Debug.Log("Jump off");
-					jumpedFromRight=true;
-					
-					Direction.y+=2f;
-					
-					Direction.x=0;
-					Direction.z=0;
-					Direction.x-=5f;
-					Direction.z+=5f;
-					Direction = transform.TransformDirection(Direction);
+			}
+				//force player to stop wallrunning 
+				if(runTimer<0){
+					isJumping=false;
+					runTimer=0.5f;
 				}
-					//tilt camera to the right
-					if((playerCamGO.transform.localEulerAngles.z>=359f&&playerCamGO.transform.localEulerAngles.z<=359.9f)||playerCamGO.transform.localEulerAngles.z<=10f)
-						playerCamGO.transform.Rotate(0,0,1);
-
-				runTimer-=Time.deltaTime;
 			}
-		}
-			//force player to stop wallrunning 
-			if(runTimer<0){
-				isJumping=false;
-				runTimer=0.5f;
+			//Turn the character to where the player is looking at after each frame.
+			playerController.Move(Direction*Time.deltaTime);
+			
+			playerTransform.localEulerAngles=new Vector3(playerTransform.localEulerAngles.x,playerRotY,playerTransform.localEulerAngles.z);
+			playerCamGO.transform.localEulerAngles= new Vector3(-(playerRotX),playerCamGO.transform.localEulerAngles.y,playerCamGO.transform.localEulerAngles.z);
+
+			RaycastHit leftRay;
+			RaycastHit rightRay;
+			
+			Debug.DrawRay(playerCamGO.transform.position,-(playerCamGO.transform.right),Color.red);
+			Debug.DrawRay(playerCamGO.transform.position,playerCamGO.transform.right,Color.green);
+			
+			//check left side if player is touching a wall
+			if(Physics.Raycast(playerCamGO.transform.position,-(playerCamGO.transform.right),out leftRay,1)&&(isJumping&&Input.GetButton("Sprint"))){
+				isWallRunningLeft=true;
+				isWallRunning=true;
+			}else{
+				isWallRunningLeft=false;
+				if(!isWallRunningRight)
+				isWallRunning=false;
 			}
+			//check right side if player is touching a wall
+			if(Physics.Raycast(playerCamGO.transform.position,playerCamGO.transform.right,out rightRay,1)&&(isJumping&&Input.GetButton("Sprint"))){
+				isWallRunningRight=true;
+				isWallRunning=true;
+			}else{
+				isWallRunningRight=false;
+				if(!isWallRunningLeft)
+				isWallRunning=false;
+			}
+			//carry items
+			CarryMethod();
+			//creating projectiles
+			ProjectileMaker();
 		}
-
-		playerController.Move(Direction*Time.deltaTime);
-		
-		playerTransform.localEulerAngles=new Vector3(playerTransform.localEulerAngles.x,playerRotY,playerTransform.localEulerAngles.z);
-		playerCamGO.transform.localEulerAngles= new Vector3(-(playerRotX),playerCamGO.transform.localEulerAngles.y,playerCamGO.transform.localEulerAngles.z);
-
-		RaycastHit leftRay;
-		RaycastHit rightRay;
-		
-		Debug.DrawRay(playerCamGO.transform.position,-(playerCamGO.transform.right),Color.red);
-		Debug.DrawRay(playerCamGO.transform.position,playerCamGO.transform.right,Color.green);
-		
-		//check left side if player is touching a wall
-		if(Physics.Raycast(playerCamGO.transform.position,-(playerCamGO.transform.right),out leftRay,1)&&(isJumping&&Input.GetButton("Sprint"))){
-			isWallRunningLeft=true;
-			isWallRunning=true;
-		}else{
-			isWallRunningLeft=false;
-			if(!isWallRunningRight)
-			isWallRunning=false;
-		}
-		//check right side if player is touching a wall
-		if(Physics.Raycast(playerCamGO.transform.position,playerCamGO.transform.right,out rightRay,1)&&(isJumping&&Input.GetButton("Sprint"))){
-			isWallRunningRight=true;
-			isWallRunning=true;
-		}else{
-			isWallRunningRight=false;
-			if(!isWallRunningLeft)
-			isWallRunning=false;
-		}
-		//carry items
-		CarryMethod();
-		//creating projectiles
-		ProjectileMaker();
 	}
 	
 	void OnCollisionStay(Collision col){

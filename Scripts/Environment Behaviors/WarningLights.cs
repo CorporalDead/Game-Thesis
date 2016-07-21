811428103e5fdcacaafaf78ebc4f6454b217922c
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-07-21 18:52:15 +0800

    Added environment script folder
    Add environment-related scripts here.

---
+++ Scripts/Environment Behaviors/WarningLights.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarningLights : MonoBehaviour {
+
+	Light CurrentLightSource;
+	// Use this for initialization
+	void Start () {
+		CurrentLightSource=GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//script taken from Unity Docs
+		float phi = Time.time / 1 * 2 * Mathf.PI;
+		float amplitude = Mathf.Cos(phi) * 1 + 1;
+		CurrentLightSource.intensity=amplitude;
+	}
+}

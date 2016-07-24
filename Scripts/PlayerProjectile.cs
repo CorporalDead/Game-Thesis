cd78249a63809f6118cc3b590ec2f4657ba77a7e
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-07-24 14:33:48 +0800

    add comments and able to destroy limbs after a certain time

--- Scripts/PlayerProjectile.cs
+++ Scripts/PlayerProjectile.cs
@@ -11,8 +11,11 @@ public class PlayerProjectile : MonoBehaviour {
 
 	public GameObject BloodSplatter;
 	public GameObject BloodParticleReference;
+	public GameObject HeadShotReference;
 
 	private GameObject blood;
+	private GameObject HeadshotSplatter;
+	private GameObject bloodSplatter;
 
 	private GameObject LeftForearm;
 	private GameObject LeftShoulder;
@@ -30,11 +33,13 @@ public class PlayerProjectile : MonoBehaviour {
 	private GameObject Chest;
 
 	private bool isHit;
+	private float BloodSplatterScale;
 
 	private Dismemberment DismemberReference;
+	private JanitorScript CleanUpReference;
 	// Use this for initialization
 	void Start () {
-	
+		CleanUpReference=GameObject.Find("ObjectCleaner").GetComponent<JanitorScript>();
 	}
 	
 	// Update is called once per frame
@@ -45,15 +50,19 @@ public class PlayerProjectile : MonoBehaviour {
 	void OnCollisionEnter(Collision col){
 		//collision for enemies
 		if(col.gameObject.tag=="Enemy"){
+			//variable that checks for rigidbodies for the collided objects
+			//we check the parents instead of the child in order to get a script so we can reference and interact with it.
 			Rigidbody[] rb=col.gameObject.GetComponentsInParent<Rigidbody>();
 			foreach(Rigidbody JointRB in rb){
-				if(JointRB.gameObject.transform.parent!=null)
+				if(JointRB.gameObject.transform.parent!=null){
 					if(JointRB.GetComponent<Dismemberment>()!=null){
 						DismemberReference=col.gameObject.GetComponentInParent<Dismemberment>();
 
+						//disable kinematics once the character dies
 						DismemberReference.setAlive(false);
+						//only dismember when the limbs have been shot multiple times
 						DismemberReference.setBodyHP(DismemberReference.getHP()-1);
-
+					
 				
 				if(DismemberReference.getHP()==0){
 					if(col.gameObject.name=="LeftForearm"){
@@ -61,6 +70,8 @@ public class PlayerProjectile : MonoBehaviour {
 							LeftForearm=Instantiate(ForearmGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
 							DismemberReference.setLeftForearmActive(true);
 							blood=Instantiate(BloodParticleReference,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+
+							Destroy(LeftForearm,5);
 							Destroy(blood,3);
 							Destroy(col.gameObject);
 					}
@@ -70,6 +81,8 @@ public class PlayerProjectile : MonoBehaviour {
 							else
 								LeftShoulder=Instantiate(ShoulderGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
 							blood=Instantiate(BloodParticleReference,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+
+							Destroy(LeftArm,5);
 							Destroy(blood,3);
 							Destroy(col.gameObject);
 					}
@@ -77,16 +90,23 @@ public class PlayerProjectile : MonoBehaviour {
 					if(col.gameObject.name=="RightForearm"){
 							RightForearm=Instantiate(ForearmGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
 							DismemberReference.setRightForearmActive(true);
+
 							blood=Instantiate(BloodParticleReference,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+
+							Destroy(RightForearm,5);
 							Destroy(blood,3);
 							Destroy(col.gameObject);
 					}
 					if(col.gameObject.name=="RightClavicle"){
+							//remove the entire arm when rightforearm is still intact
 						if(!DismemberReference.getRightForearmActive())
 							RightArm=Instantiate(ArmGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
-							else
+							else //remove clavicle/shoulder if the rightforearm is not intact
 								RightShoulder=Instantiate(ShoulderGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+
 							blood=Instantiate(BloodParticleReference,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+
+							Destroy(RightArm,5);
 							Destroy(blood,3);
 							Destroy(col.gameObject);
 					}
@@ -95,6 +115,8 @@ public class PlayerProjectile : MonoBehaviour {
 						if(DismemberReference.getLeftForearmActive()&&DismemberReference.getRightForearmActive()){
 							Chest=Instantiate(ChestGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
 								blood=Instantiate(BloodParticleReference,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+
+								Destroy(Chest,5);
 								Destroy(blood,3);
 								Destroy(col.gameObject);
 						}
@@ -108,32 +130,38 @@ public class PlayerProjectile : MonoBehaviour {
 					if(col.gameObject.name=="LeftLeg"){
 							LeftUpperLeg=Instantiate(UpperLegGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
 							blood=Instantiate(BloodParticleReference,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+
+							Destroy(LeftUpperLeg,5);
 							Destroy(blood,3);
 							Destroy(col.gameObject);
 					}
 					if(col.gameObject.name=="RightLeg"){
 							RightUpperLeg=Instantiate(UpperLegGib,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
 							blood=Instantiate(BloodParticleReference,col.gameObject.transform.position,col.gameObject.transform.rotation) as GameObject;
+
+							Destroy(RightUpperLeg,5);
 							Destroy(blood,3);
 							Destroy(col.gameObject);
 					}
 				}
 			}
 		}
+	}
+			//raycasting for blood sprites
 			RaycastHit bloodRay;
-			
-			Debug.Log("I love pkmns"+gameObject.name);
-			
-			Debug.DrawRay(transform.position,-col.transform.forward,Color.cyan);
-			
-			if(Physics.Raycast(transform.position,-col.transform.forward,out bloodRay,3f)){
+			//cast a ray on the backside of an object
+			if(Physics.Raycast(transform.position,-col.transform.forward,out bloodRay,2f)){
 
 				if(bloodRay.collider.tag=="BloodSurface"){
-					Instantiate(BloodSplatter,bloodRay.point,Quaternion.FromToRotation(Vector3.up, bloodRay.normal));
-					Debug.Log(bloodRay.normal);
+					BloodSplatterScale=Random.Range(0.05f,0.2f);
+					bloodSplatter=Instantiate(BloodSplatter,bloodRay.point,Quaternion.FromToRotation(Vector3.up, bloodRay.normal)) as GameObject;
+					//change scale of instantiated bloodsplatters in order to add more variety
+					bloodSplatter.transform.localScale=new Vector3(BloodSplatterScale,BloodSplatterScale,BloodSplatterScale);
+					//increment janitor script in order to delete the blood sprites once it reaches a certain number (varies depending on player settings)
+					CleanUpReference.BloodSpriteCTR++;
 				}
-			}	
-			Destroy(gameObject);
+			}
 		}
+		Destroy(gameObject);
 	}
 }

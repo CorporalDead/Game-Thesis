0ef69074625a76fb4265f86f1f8a33cbab0a1f8f
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-07-24 14:15:53 +0800

    Added script that deletes sprites and giblets

---
+++ Scripts/Environment Behaviors/JanitorScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JanitorScript : MonoBehaviour {
+
+	public int BloodSpriteCTR;
+
+	private PlayerProjectile projectileReference;
+
+	private List<GameObject> bloodSprites;
+	// Use this for initialization
+	void Start () {
+		bloodSprites=new List<GameObject>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(BloodSpriteCTR>32){
+			Destroy(GameObject.FindGameObjectWithTag("BloodSplatter"));
+			BloodSpriteCTR--;
+		}
+		Debug.Log("# of Splats: "+BloodSpriteCTR);
+	}
+}

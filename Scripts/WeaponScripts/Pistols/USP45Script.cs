55641f5b6cd68e3a9a3724557994a5bb2c334ca6
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-07-30 02:58:29 +0800

    Added USP45 script

---
+++ Scripts/WeaponScripts/Pistols/USP45Script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class USP45Script:Weapon,IFireArms {
+	Weapon WeaponReference;
+	IFireArms test;
+	// Use this for initialization
+	void Start(){
+		WeaponReference = new Weapon();
+		test = new USP45Script();
+	}
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Child:"+WeaponReference.damage+"Daddy:"+base.damage);
+		Shoot();
+	}
+	public void Shoot(){
+		WeaponReference.damage=5f;	
+	}
+	public void Reload(){
+		Debug.Log("FUCK MEEEE; )))");
+	}
+	public void RecoilPattern(){
+	
+	}
+}

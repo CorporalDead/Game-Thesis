4cb941e3ed6952cecda3efc60371b834b0eb5b75
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-07-30 02:57:48 +0800

    Added Weapon Parent

---
+++ Scripts/WeaponScripts/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+	internal AudioSource weaponFireClip;
+	internal float damage;
+	internal float rateOfFire;
+	internal float recoilModifier;
+
+	void Update(){
+		Debug.Log("Parent: "+damage);
+	}
+	internal string Message(){
+		return	"Parent: "+damage; 
+	}
+}

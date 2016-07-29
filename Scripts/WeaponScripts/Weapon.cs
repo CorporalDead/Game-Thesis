533761b755707645a7b93160b89034798ac11799
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-07-30 03:55:16 +0800

    Added shoot method

--- Scripts/WeaponScripts/Weapon.cs
+++ Scripts/WeaponScripts/Weapon.cs
@@ -1,16 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Weapon : MonoBehaviour {
+public class Weapon : MonoBehaviour,IFireArms {
 	internal AudioSource weaponFireClip;
 	internal float damage;
 	internal float rateOfFire;
+	internal float delayFireTime;
+	internal float reloadTime;
 	internal float recoilModifier;
 
 	void Update(){
-		Debug.Log("Parent: "+damage);
+
 	}
-	internal string Message(){
-		return	"Parent: "+damage; 
+	public void Shoot(float damage,float rateOfFire, float recoilModifier, bool isSemiAuto){
+		if(isSemiAuto){
+			if(delayFireTime>rateOfFire){
+				if(Input.GetKeyDown(KeyCode.Mouse0)){
+					Debug.Log("bang");
+
+					delayFireTime=0;
+				}
+			}
+		}
+		delayFireTime++;
+	}
+	public void Reload(){
+		
+	}
+	public void RecoilPattern(){
+		
+	}
+	public void Aim(){
+		
 	}
 }

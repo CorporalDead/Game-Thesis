5f421e0318e227f9e39aba01d7d5107d2368ea0a
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-07-31 21:52:56 +0800

    Added SMG scripts

---
+++ Scripts/WeaponScripts/SMG/KrissVectorScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KrissVectorScript : Primary {
+	Weapon WeaponReference;
+	// Use this for initialization
+	void Start () {
+		WeaponReference=new Primary();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		WeaponReference.Shoot (5000,2,5,false);
+	}
+}

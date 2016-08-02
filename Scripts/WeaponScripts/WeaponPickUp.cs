ec424ef41cb93c01faa93bd7d9b1035f5eb6aa03
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-08-02 20:51:59 +0800

    Players can pick up items by walking over them

--- Scripts/WeaponScripts/WeaponPickUp.cs
+++ Scripts/WeaponScripts/WeaponPickUp.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class WeaponPickUp : MonoBehaviour {
+	DroppedWeaponScript	 DroppedWeaponScriptReference;
 
 	private Transform Primary;
 	private Transform Secondary;
@@ -14,37 +15,44 @@ public class WeaponPickUp : MonoBehaviour {
 	private MonoBehaviour PickedUpWeaponInheritance;
 
 	private string WeaponName;
+	private bool isPickUp;
+
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKeyDown(KeyCode.E))
-			PickUp();
-
 		WeaponSwapping();
+
+		//Debug.Log(isPickUp);
 	}
 
-	void PickUp(){
-		RaycastHit hit;
 
-			if(Physics.Raycast(transform.position,transform.forward,out hit,2f)){
-				Transform[] ParentTrans=hit.collider.GetComponentsInParent<Transform>();
-					foreach(Transform childTrans in ParentTrans){
-						if(childTrans.gameObject.transform.parent==null){
-							if(childTrans.tag=="PickUpWep"){
-								Debug.Log(childTrans.name);
-								
-								Transform PickedUpWeapon=transform.FindChild(childTrans.name);
-								PickedUpWeaponInheritance=PickedUpWeapon.GetComponent<MonoBehaviour>();
-								
-								if(PickedUpWeaponInheritance is Primary)
-									Primary=PickedUpWeapon;
-								if(PickedUpWeaponInheritance is Secondary)
-									Secondary=PickedUpWeapon;
-					}
-				}
-			}
-		}
-	}
+	//Pick Up by Raycast
+//	void PickUp(){
+//		RaycastHit hit;
+//
+//			if(Physics.Raycast(transform.position,transform.forward,out hit,2f)){
+//				Transform[] ParentTrans=hit.collider.GetComponentsInParent<Transform>();
+//					foreach(Transform childTrans in ParentTrans){
+//						if(childTrans.gameObject.transform.parent==null){
+//							if(childTrans.tag=="PickUpWep"){
+//						
+//								isPickUp=true;
+//							
+//								if(Input.GetKeyDown(KeyCode.E)){	
+//								Transform PickedUpWeapon=transform.FindChild(childTrans.name);
+//								PickedUpWeaponInheritance=PickedUpWeapon.GetComponent<MonoBehaviour>();
+//								
+//								if(PickedUpWeaponInheritance is Primary)
+//									Primary=PickedUpWeapon;
+//								if(PickedUpWeaponInheritance is Secondary)
+//									Secondary=PickedUpWeapon;
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+
 
 	void WeaponSwapping(){
 		if(Input.GetKeyDown(KeyCode.Alpha1)){
@@ -68,5 +76,36 @@ public class WeaponPickUp : MonoBehaviour {
 //				Equipment.gameObject.SetActive(false);
 			}
 		}
+	}
+
+	void OnCollisionEnter(Collision col){
+
+		if(col.gameObject.tag=="PickUpWep"){
+		Transform[] ParentTrans=col.gameObject.GetComponentsInParent<Transform>();
+		foreach(Transform childTrans in ParentTrans){
+			if(childTrans.gameObject.transform.parent==null){
+
+					Debug.Log("Collided Weapon:"+childTrans.name);
+					isPickUp=true;
+					
+					if(Input.GetKeyDown(KeyCode.E)){	
+						Transform PickedUpWeapon=GameObject.Find("PlayerCamera").transform.FindChild(childTrans.name);
+						PickedUpWeaponInheritance=PickedUpWeapon.GetComponent<MonoBehaviour>();
+
+						if(PickedUpWeaponInheritance is Primary)
+							Primary=PickedUpWeapon;
+						if(PickedUpWeaponInheritance is Secondary)
+							Secondary=PickedUpWeapon;
+
+						Destroy(childTrans.gameObject);
+				}
+			}
+		}
+	}else
+			isPickUp=false;
+}
+	void OnGUI(){
+		if(isPickUp)
+		GUI.Label(new Rect(Screen.width/2,Screen.height/2,200,30),"Pick Up?");
 	}
 }

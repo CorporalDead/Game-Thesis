29a062c50b76955dd38ca373c97198998947cac6
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-08-05 15:53:08 +0800

    Added ammo counter and reloading

--- Scripts/WeaponScripts/Weapon.cs
+++ Scripts/WeaponScripts/Weapon.cs
@@ -7,27 +7,43 @@ public class Weapon : MonoBehaviour,IFireArms {
 	private GameObject PlayerProjectile;
 
 	internal AudioSource weaponFireClip;
+	internal bool isEmpty;
 	internal float damage;
 	internal float rateOfFire;
 	internal float delayFireTime;
 	internal float reloadTime;
 	internal float recoilModifier;
-	
+	internal int MagazineCapacity;
+	internal int AmmoSum;
+
 	public void Shoot(float force,float damage,float rateOfFire, bool isSemiAuto){
-		if(isSemiAuto){
+		if(isSemiAuto&&MagazineCapacity!=0){
 			if(delayFireTime>rateOfFire){
 				if(Input.GetKeyDown(KeyCode.Mouse0)){
 					PlayerProjectile=Instantiate(Resources.Load("Projectile"),GameObject.Find("Barrel").transform.position,Quaternion.identity) as GameObject;
 					PlayerProjectile.GetComponent<Rigidbody>().AddForce(GameObject.Find("Barrel").transform.forward*force);
 
 					delayFireTime=0;
+					MagazineCapacity--;
+				}
+			}
+		}else
+			if(!isSemiAuto&&MagazineCapacity!=0){
+				if(delayFireTime>rateOfFire){
+					if(Input.GetKey(KeyCode.Mouse0)){
+						PlayerProjectile=Instantiate(Resources.Load("Projectile"),GameObject.Find("Barrel").transform.position,Quaternion.identity) as GameObject;
+						PlayerProjectile.GetComponent<Rigidbody>().AddForce(GameObject.Find("Barrel").transform.forward*force);
+					
+						delayFireTime=0;
+						MagazineCapacity--;
 				}
 			}
 		}
 		delayFireTime++;
 	}
 	public void Reload(){
-		
+		if(MagazineCapacity==0)
+			isEmpty=true;
 	}
 	public void RecoilPattern(){
 		

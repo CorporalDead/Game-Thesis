0ca690ca118df4ff3e1a67ccea60d6d8c61916ea
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-08-05 15:54:23 +0800

    fixed inheritance
    script takes directly from parent rather than creating a reference

--- Scripts/WeaponScripts/Pistols/USP45Script.cs
+++ Scripts/WeaponScripts/Pistols/USP45Script.cs
@@ -2,15 +2,19 @@
 using System.Collections;
 
 public class USP45Script:Secondary{
-	Weapon WeaponReference;
+
 	// Use this for initialization
 	void Start(){
-		WeaponReference = new Secondary();
-
+		MagazineCapacity=12;
+		AmmoSum=120;
 	}
 	// Update is called once per frame
 	void Update () {
-		WeaponReference.Shoot(3000,1,10,true); //Shoot(float force,float damage, float rateOfFire, bool isSemiAuto);
-	
+		Shoot(3000,1,10,true); //Shoot(int MagazineCapacity, int ammocapacity,float force,float damage, float rateOfFire, bool isSemiAuto);
+	//	WeaponReference.Reload(30,90); //Reload(int magazinecapacity, int ammocount);
+		if(Input.GetKeyDown(KeyCode.R))
+			Reload();
+
+		Debug.Log(""+MagazineCapacity);
 	}
 }

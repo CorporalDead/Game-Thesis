a649bfd5d11f74727783fba46fc60e05f4406ad2
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2016-08-05 15:52:32 +0800

    Applied force to projectile

--- Scripts/WeaponScripts/IFireArms.cs
+++ Scripts/WeaponScripts/IFireArms.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using System.Collections;
 
 public interface IFireArms{
-	void Shoot(float damage,float rateOfFire,float recoilModifier, bool isSemiAuto);
+	void Shoot(float force,float damage,float rateOfFire, bool isSemiAuto);
 	void Reload();
 	void Aim();
 	void RecoilPattern();
